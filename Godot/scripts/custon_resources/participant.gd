4431feb5a1ab22874c6801887985baa6dd7cf825
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2026-05-22 23:17:09 +0200

    Godot: add extra field to participant resource.

--- Godot/scripts/custon_resources/participant.gd
+++ Godot/scripts/custon_resources/participant.gd
@@ -5,3 +5,7 @@ class_name Participant
 @export var persona_id: String
 @export var look_target: Vector3
 @export var icon: Texture2D
+
+@export_category("extra")
+@export var birth_date: String = "Unkown"
+@export var likes: String = "Unkown"
